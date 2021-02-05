a13456da339165225cc1c7b5d1f339aad27f0179
Dan Ngetich <dancanmilgo73@gmail.com>
2021-02-05 16:45:07 +0300

    data filtering and search buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import Jobs from "./components/Jobs";
 
+const initialValues = {
+  searchInput: "",
+  Seniority: "",
+  Location: "",
+  selectedRadio: "",
+};
+
 function App() {
+  const [values, setValues] = useState(initialValues);
   async function getJobs() {
     const res = await fetch("https://job-listing-server.herokuapp.com/");
     var jobs = await res.json();
@@ -14,6 +22,15 @@ function App() {
   useEffect(() => {
     getJobs();
   }, []);
+  //handle change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="App">
       <div className="page-top">
@@ -25,13 +42,64 @@ function App() {
             className="search-bar"
             type="text"
             placeholder="Search..."
-            name="titleInput"
+            name="searchInput"
+            onChange={handleChange}
           />
-          <input type="submit" className="btn btn-success" value="Submit" />
+          <button
+            type="submit"
+            className="search-button btn btn-success"
+            value="Submit"
+          >
+            <i className="bi bi-arrow-clockwise"></i>
+          </button>
+          <>
+            <i className="bi bi-arrow-clockwise"></i>
+          </>
         </form>
       </div>
-
-      <Jobs jobs={jobList} />
+      <div>
+        <div className="select-field">
+          <select onChange={handleChange} defaultValue="" name="Seniority">
+            <option value="">Seniority</option>
+            <option value="Entry_level">Entry Level</option>
+            <option value="Mid_level">Mid-level</option>
+            <option value="Senior_level">Senior-level</option>
+          </select>
+          <select onChange={handleChange} defaultValue="" name="Location">
+            <option value="">Location</option>
+            <option value="World">World</option>
+            <option value="Remote">Remote</option>
+            <option value="Kenya">Kenya</option>
+          </select>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="selectedRadio"
+              value="Full time"
+              onChange={handleChange}
+            />
+            <label className="form-check-label">Full Time</label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="selectedRadio"
+              value="Part time"
+              onChange={handleChange}
+            />
+            <label className="form-check-label">Part Time</label>
+          </div>
+        </div>
+      </div>
+      <Jobs
+        jobs={jobList}
+        search={values.searchInput}
+        seniority={values.Seniority}
+        location={values.Location}
+        type={values.selectedRadio}
+      />
     </div>
   );
 }
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,14 +1,28 @@
 import React from "react";
 import Job from "./Job";
 
-function Jobs({ jobs }) {
+function Jobs({ jobs, search, seniority, location, type }) {
+  if (search.length > 0) {
+    jobs = jobs.filter((job) =>
+      job.title.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+  if (location.length > 0) {
+    jobs = jobs.filter((job) =>
+      job.location.toLowerCase().includes(location.toLowerCase())
+    );
+  }
+  if (type.length > 0) {
+    jobs = jobs.filter((job) =>
+      job.type.toLowerCase().includes(type.toLowerCase())
+    );
+  }
+
   return (
     <div className="outer">
       <div className="JobsList">
         {jobs.map((job) => (
-          <li key={job.description}>
-            <Job job={job} />
-          </li>
+          <Job job={job} />
         ))}
       </div>
     </div>
